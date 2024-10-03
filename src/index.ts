ba3171fa8302ecf308932bd42f243ce4aa772e7d
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2024-10-03 23:00:30 +0530

    server: basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello, World');
+import { config } from 'dotenv';
+import { expand } from 'dotenv-expand';
+import express, { Request, Response } from 'express';
+
+expand(config());
+
+const app = express();
+
+const PORT = process.env.PORT || 2000;
+
+app.use('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello, World!</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on http://localhost:${PORT}`);
+});
