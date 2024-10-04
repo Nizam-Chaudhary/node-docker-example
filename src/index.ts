f2c55f67280f47d56d9b96f231deb93857c41559
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2024-10-05 00:45:20 +0530

    add: mongo db and connect with it

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { config } from 'dotenv';
 import { expand } from 'dotenv-expand';
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 expand(config());
 
 const app = express();
+
+mongoose
+  .connect('mongodb://nizam:nizam@mongo:27017/?authSource=admin')
+  .then(() => console.log('Successfully connected with DB'))
+  .catch((e) => console.error(e));
 
 const PORT = process.env.PORT || 3000;
 
